c8ece99be7bb05bcf53605165c7ac8b5affaa04e
Spitfire-Ice <37056025+Spitfire-Ice@users.noreply.github.com>
2020-02-21 19:39:02 +0200

    base

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is empty React project(webpack)</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
